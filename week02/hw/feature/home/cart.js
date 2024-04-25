6f8346b8bb3e765764934ec27798253937537be9
ivoryeee <psa109@sookmyung.ac.kr>
2024-04-25 16:30:31 +0900

    [refactor] DOMContentLoaded 삭제

--- week02/hw/feature/home/cart.js
+++ week02/hw/feature/home/cart.js
@@ -62,6 +62,4 @@ navCart.addEventListener("click", () => {
   window.location.href = "../pages/cart.html";
 });
 
-window.addEventListener("DOMContentLoaded", function () {
-  addProductAlert();
-});
+addProductAlert();
