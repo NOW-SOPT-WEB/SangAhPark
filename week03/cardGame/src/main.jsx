b6943c13c2d4a134edfcf4722e57f0a4d22823f1
ivoryeee <psa109@sookmyung.ac.kr>
2024-04-27 11:23:12 +0900

    [init] GlobalStyle 설치 및 추가

--- week03/cardGame/src/main.jsx
+++ week03/cardGame/src/main.jsx
@@ -1,10 +1,15 @@
+import { Global } from "@emotion/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import globalStyle from "./style/GlobalStyle";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <Global styles={globalStyle} />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </>
 );
