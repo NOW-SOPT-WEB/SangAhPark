179d76a3a66c23f96ef20d426f21db73b37b8b6e
ivoryeee <psa109@sookmyung.ac.kr>
2024-04-29 15:41:46 +0900

    [feat] 카드 뒤집기 기본 기능 구현

--- week03/cardGame/src/components/Card.jsx
+++ week03/cardGame/src/components/Card.jsx
@@ -1,5 +1,7 @@
 import { CARD_LIST } from "./../constants/cardList";
 import styled from "@emotion/styled";
+import { useState, useEffect } from "react";
+import dummyImage from "../../public/img/sanrio.jpg";
 
 const getRandomCards = () => {
   const shuffledCards = [...CARD_LIST].sort(() => Math.random() - 0.5);
@@ -9,10 +11,60 @@ const getRandomCards = () => {
 };
 
 const Card = () => {
-  const randomCards = getRandomCards();
-  return randomCards.map((card, index) => (
-    <CardWrapper key={index}>
-      <CardImg src={card.imgSrc} alt={card.imgAlt}></CardImg>
+  const [cards, setCards] = useState(
+    getRandomCards().map((card) => ({ ...card, flipped: false }))
+  );
+  const [selectedIndexes, setSelectedIndexes] = useState([]);
+
+  useEffect(() => {
+    const flipBackCards = () => {
+      const updatedCards = cards.map((card, index) => ({
+        ...card,
+        flipped: false,
+      }));
+      setCards(updatedCards);
+      setSelectedIndexes([]);
+    };
+
+    if (selectedIndexes.length === 2) {
+      const firstIndex = selectedIndexes[0];
+      const secondIndex = selectedIndexes[1];
+      if (cards[firstIndex].imgSrc !== cards[secondIndex].imgSrc) {
+        console.log("First card" + cards[firstIndex].imgSrc);
+        console.log("Seconde card" + cards[secondIndex].imgSrc);
+        console.log("different card");
+        setTimeout(() => {
+          flipBackCards();
+        }, 1000);
+      } else {
+        console.log("First card" + cards[firstIndex].imgSrc);
+        console.log("Seconde card" + cards[secondIndex].imgSrc);
+
+        console.log("same card");
+        setSelectedIndexes([]);
+      }
+    }
+  }, [cards, selectedIndexes]);
+
+  const handleCardClick = (index) => {
+    if (selectedIndexes.length < 2 && !selectedIndexes.includes(index)) {
+      const updatedCards = cards.map((card, i) =>
+        i === index ? { ...card, flipped: true } : card
+      );
+      /*       const updatedCards = [...cards];
+      updatedCards[index].flipped = !updatedCards[index].flipped;
+ */ setCards(updatedCards);
+      setSelectedIndexes([...selectedIndexes, index]);
+    }
+  };
+
+  return cards.map((card, index) => (
+    <CardWrapper key={index} onClick={() => handleCardClick(index)}>
+      <CardImg
+        src={card.flipped ? card.imgSrc : dummyImage}
+        alt={card.imgAlt}
+        flipped={card.flipped}
+      ></CardImg>
     </CardWrapper>
   ));
 };
@@ -32,4 +84,8 @@ const CardWrapper = styled.div`
 const CardImg = styled.img`
   width: 10rem;
   height: 10rem;
+  transition: transform 0.3s ease;
+  transform-style: preserve-3d;
+  cursor: pointer;
+  ${({ flipped }) => flipped && "transform: rotateY(180deg);"}
 `;
