5711513bbfa7bea216536cdd60f35f3b90279726
ivoryeee <psa109@sookmyung.ac.kr>
2024-05-09 22:26:35 +0900

    [style] input 컴포넌트 grid -> flex로 수정

--- week04/hw/src/components/FormInput.tsx
+++ week04/hw/src/components/FormInput.tsx
@@ -20,24 +20,24 @@ const FormInput = (props: Props) => {
 export default FormInput;
 
 const FormInputWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 10rem 10rem;
+  display: flex;
   align-items: center;
-  justify-content: center;
   height: 3rem;
   padding-top: 1rem;
 `;
 
 const FormInputTitle = styled.p`
-  grid-column: 1;
-  align-items: center;
-  justify-content: center;
+  flex: 20%;
   height: 2rem;
+  margin-right: 0;
+  padding-right: 0;
 
   font-size: 1rem;
 `;
 
 const FormInputBox = styled.input`
+  flex: 40%;
   height: 2rem;
+  margin-right: 3rem;
   padding-left: 0;
 `;
