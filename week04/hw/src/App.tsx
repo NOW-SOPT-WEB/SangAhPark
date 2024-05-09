8b9bfb817a9a6afd80040ab72cece3b4c092a361
ivoryeee <psa109@sookmyung.ac.kr>
2024-05-09 18:07:00 +0900

    [feat] 라우터 설정

--- week04/hw/src/App.tsx
+++ week04/hw/src/App.tsx
@@ -1,11 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Login from '../src/pages/Login';
 import Register from '../src/pages/Register';
 
 function App() {
   return (
     <>
-      {/*     <Login /> */}
-      <Register />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
